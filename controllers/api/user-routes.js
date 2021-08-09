144d2d2bc6b63f59f7e48aa60a62a097673c1e62
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-08 22:12:01 -0700

    logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,6 +60,16 @@ router.post('/login', (req, res) => {
         })
 })
 
+// User logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
+})
 
 
 module.exports = router;
