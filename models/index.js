76f7113db36d95f30b1d014314415a10967fce1e
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-09 19:45:49 -0700

    add comments model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
+// One to Many (Users have many posts)
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -10,4 +12,24 @@ Post.belongsTo(User, {
     onDelete: 'CASCADE'
 });
 
-module.exports = { User, Post }
+// One to Many (Users have many comments)
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+// One to Many (Posts have many comments)
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment }
