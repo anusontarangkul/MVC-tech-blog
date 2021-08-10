f299612dca30673d66ea35fcb8e3a35b228b3784
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-09 23:15:57 -0700

    delete and update post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,6 +3,8 @@ const { User, Post } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
+// route /api/posts/
+
 // Get all Post
 router.get('/', (req, res) => {
     Post.findAll({
@@ -31,6 +33,49 @@ router.post('/', withAuth, (req, res) => {
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
+        })
+})
+
+// Update Post
+router.put('/:id', (req, res) => {
+    Post.update({
+        title: req.body.title,
+        content: req.body.content
+    },
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                return res.status(404).json({ message: 'No post found with this id.' });
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+
+// Delete Post
+
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                return res.status(404).json({ message: 'No post found with this id.' });
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
         })
 })
 
