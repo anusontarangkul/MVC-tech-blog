ac56d886ba8c3e93f170f165d3f594e83e120abd
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-10 23:47:28 -0700

    style your posts

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,7 +1,7 @@
 async function addPost(event) {
     event.preventDefault();
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="post-content"]').value.trim();
+    const content = document.querySelector('textarea[name="post-content"]').value.trim();
 
     const response = await fetch('/api/posts', {
         method: 'POST',
@@ -15,7 +15,7 @@ async function addPost(event) {
     });
 
     document.querySelector('input[name="post-title"]').value = "";
-    document.querySelector('input[name="post-content"]').value = "";
+    document.querySelector('textarea[name="post-content"]').value = "";
 
     if (response.ok) {
         document.location.replace('/dashboard');
